5582f00f2503ba1816d9561fbb79b107a40c3e12
yoannBonge <yoannbonge@live.fr>
2023-09-02 13:48:16 +0200

    delete-projects

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,4 +1,4 @@
-const modalContainer = document.getElementById("user-modal");
+const gallery = document.querySelector(".gallery");
 const modalTriggers = document.querySelectorAll(".modal-trigger");
 const modalGallery = document.querySelector(".modal-gallery");
 
@@ -15,14 +15,15 @@ function toggleModal() {
 
 //============================PARTIE PROJETS MODALE============================
 
-async function getProjects() {
+async function getProjectsInModal() {
   const response = await fetch("http://localhost:5678/api/works");
   const data = await response.json();
   displayProjectsInModal(data);
 }
 
 function displayProjectsInModal(works) {
-  for (project of works) {
+  modalGallery.innerHTML = "";
+  works.forEach((project) => {
     const projectBox = document.createElement("div");
     projectBox.classList.add("modal-project");
 
@@ -30,8 +31,8 @@ function displayProjectsInModal(works) {
     imageProject.classList.add("modal-img");
     imageProject.src = project.imageUrl;
 
-    const deleteProject = document.createElement("div");
-    deleteProject.className = "delete-project";
+    const deleteProjectIcon = document.createElement("div");
+    deleteProjectIcon.className = "delete-project";
 
     const trashCanIcon = document.createElement("i");
     trashCanIcon.className = "fa-solid fa-trash-can";
@@ -39,17 +40,61 @@ function displayProjectsInModal(works) {
     const squareIcon = document.createElement("i");
     squareIcon.className = "fa-solid fa-square";
 
-    deleteProject.append(trashCanIcon, squareIcon);
+    deleteProjectIcon.append(trashCanIcon, squareIcon);
+    deleteProjectIcon.setAttribute("id", project.id);
+    deleteProjectIcon.addEventListener("click", (event) => {
+      event.preventDefault();
+      const projectId = event.currentTarget.getAttribute("id");
+      const confirmation = window.confirm(
+        "Êtes-vous sûr de vouloir supprimer ce projet du site ?"
+      );
+
+      if (confirmation) {
+        deleteProject(projectId);
+        alert("Projet supprimé du site");
+      } else {
+        return;
+      }
+    });
 
     const edit = document.createElement("div");
     edit.classList.add("modal-edit");
     edit.textContent = "éditer";
 
     projectBox.appendChild(imageProject);
-    projectBox.appendChild(deleteProject);
+    projectBox.appendChild(deleteProjectIcon);
     projectBox.appendChild(edit);
     modalGallery.appendChild(projectBox);
+  });
+}
+
+async function deleteProject(projectId) {
+  const token = localStorage.getItem("token");
+  const response = await fetch(`http://localhost:5678/api/works/${projectId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    const deletedProjectModal = document.querySelector(
+      `.modal-project[id="${projectId}"]`
+    );
+    if (deletedProjectModal) {
+      deletedProjectModal.remove();
+
+      const deletedProjectGallery = document.querySelector(
+        `.project[id="${projectId}"]`
+      );
+      if (deletedProjectGallery) {
+        deletedProjectGallery.remove();
+      }
+    }
+  } else {
+    console.log("Échec de la tentative de suppression de la ressource");
   }
 }
 
-getProjects();
+getProjectsInModal();
